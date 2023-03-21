77a1ba163564424b8d1b324e468ed743e82a96f1
Steven <stzheng716@gmail.com>
2023-03-21 09:50:43 -0700

    refactore and update styles

--- demo/memory-game.js
+++ demo/memory-game.js
@@ -3,60 +3,57 @@
 /** Memory game: find matching pairs of cards and flip both of them. */
 
 const FOUND_MATCH_WAIT_MSECS = 1000;
-// const COLORS = [
-//   "red", "blue", "green", "orange", "purple", "black", "yellow"
-// ];
-
-// function randomRGB(){
-//   const r = Math.floor(Math.random() * 256)
-//   const g = Math.floor(Math.random() * 256)
-//   const b = Math.floor(Math.random() * 256)
-//   return `rgb(${r},${g},${b})`
-// }
-
-const numOfCards = 1;
-
-const newDeck = [];
-for(let i = 0; i < numOfCards; i++){
-  // let random = randomRGB()
-  newDeck.push(`img/card${i}.png`, `img/card${i}.png`)
-}
-
-const colors = shuffle(newDeck);
-
-const startBtn = document.querySelector("#start");
-startBtn.addEventListener("click", function () {
-  createCards(colors);
-  //added this so that start couldn't be called multiple times
-  gameOverMsg.remove()
-  this.remove()
-})
-
-const resetBtn = document.createElement("button");
-resetBtn.innerText = "RESET"
-resetBtn.addEventListener("click", function () {
-  reset();
-})
-
-const body = document.querySelector("body");
-const gameBoard = document.getElementById("game");
-
+const numOfPairs = 12;
+const bestScore = localStorage.getItem("Best Score")
 let SCORE = 0;
 let MATCHES = 0;
-let gameOverMsg = document.createElement("p")
-const score = document.querySelector("h2");
 
+const newDeck = [];
+const colors = shuffle(newDeck);
+const startBtn = document.querySelector("#start");
+const resetBtn = document.createElement("button");
+const gameOverMsg = document.createElement("p");
+resetBtn.innerText = "RESET"
+const body = document.querySelector("body");
+const gameBoard = document.getElementById("game");
+const score = document.createElement("h2");
+const gameName = document.querySelector("h1");
+const topScore = document.querySelector("#topScore");
+const topScoreEl = document.querySelector("#topScoreEl");
 
+if (bestScore) {
+  topScore.innerHTML = bestScore;
+}
+
+startBtn.addEventListener("click", function () {
+  gameBoard.append(score)
+  score.innerText = SCORE;
+  createCards(colors);
+  gameName.remove();
+  topScoreEl.remove();
+  this.remove();
+})
+
+resetBtn.addEventListener("click", function () {
+  gameBoard.innerHTML = "";
+  shuffle(newDeck);
+  SCORE = 0;
+  MATCHES = 0;
+  score.innerText = SCORE;
+  gameBoard.append(score)
+  resetBtn.remove();
+  gameOverMsg.remove();
+  createCards(colors);
+})
+
+for (let i = 0; i < numOfPairs; i++) {
+  newDeck.push(`img/card${i}.png`, `img/card${i}.png`)
+}
 
 
 /** Shuffle array items in-place and return shuffled array. */
 
 function shuffle(items) {
-  // This algorithm does a "perfect shuffle", where there won't be any
-  // statistical bias in the shuffle (many naive attempts to shuffle end up not
-  // be a fair shuffle). This is called the Fisher-Yates shuffle algorithm; if
-  // you're interested, you can learn about it, but it's not important.
-
   for (let i = items.length - 1; i > 0; i--) {
     // generate a random index between 0 and i
     let j = Math.floor(Math.random() * i);
@@ -65,6 +62,8 @@ function shuffle(items) {
   }
   return items;
 }
+
+shuffle(newDeck);
 
 /** Create card for every color in colors (each will appear twice)
  *
@@ -130,27 +129,37 @@ function cardReset() {
   SECONDCARD = undefined;
 }
 
+
 function matches() {
   MATCHES += 2
   if (MATCHES === newDeck.length) {
-    body.append(resetBtn)
-    let bestScore = localStorage.getItem("Best Score")
     if (SCORE < bestScore || bestScore === null) {
       localStorage.setItem("Best Score", SCORE)
     }
-    gameOverMsg.innerText = `Your score was ${SCORE}, the lowest Score was ${bestScore}`
+    gameOverMsg.innerText = `Your score was ${SCORE}, the best score was ${bestScore}`
     body.append(gameOverMsg)
+    const resetDiv = document.createElement("div");
+    resetDiv.append(resetBtn)
+    body.append(resetDiv)
   }
   cardReset()
 }
 
-function reset() {
-  body.append(startBtn)
 
-  gameBoard.innerHTML = ""
-  shuffle(newDeck)
-  SCORE = 0;
-  MATCHES = 0;
-  score.innerText = SCORE;
-  resetBtn.remove();
-}
+//TODO LIST
+
+//add game name back at reset?
+//add score back once reset button is pressed
+//deploy the add on aws S3
+
+
+// const COLORS = [
+//   "red", "blue", "green", "orange", "purple", "black", "yellow"
+// ];
+
+// function randomRGB(){
+//   const r = Math.floor(Math.random() * 256)
+//   const g = Math.floor(Math.random() * 256)
+//   const b = Math.floor(Math.random() * 256)
+//   return `rgb(${r},${g},${b})`
+// }
